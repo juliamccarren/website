275cd2ddc9ed1221a77987162f952a00d13024ea
Julia McCarren <giulia.mc.carren@gmail.com>
2026-02-26 18:00:55 +0100

    V23

--- js/VersionCore.js
+++ js/VersionCore.js
@@ -1,6 +1,6 @@
 
 class VersionCore {
-    static info = {"number": "22", "hash": "DB5DD7"};
+    static info = {"number": "23", "hash": "D91826"};
     
     static get display() {
         return `CORE_V${this.info.number} [${this.info.hash}]`;
